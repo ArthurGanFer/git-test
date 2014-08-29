f608d7b7bbc67abc418860a60f20adbf10441db9
31338283 <31338283@087868-P33S105.macklabs.br>
2014-08-29 08:55:53 -0300

    Exercicio 5.1 INCOMPLETO

---
+++ Array.php
@@ -0,0 +1,30 @@
+<?php
+$turma [0] = "02K01";
+$turma [1] = "02J01";
+$turma [2] = "02K01";
+
+$dis [0] = "Tecnologia Web II";
+$dis [1] = "Linguagem de Programacao I";
+$dis [2] = "Linguagem de Programacao I";
+
+$semana [0] = "segunda";
+$semana [1] = "terca";
+$semana [2] = "quarta";
+
+$prof [0] = "Charles";
+$prof [1] = "Joao";
+$prof [2] = "Charles";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
