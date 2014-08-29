874d0bbccceb2f1826727c53fa7b47d77e0c0780
31338283 <31338283@087868-P33S105.macklabs.br>
2014-08-29 08:13:59 -0300

    Tabela zebrada php FINALIZADO

--- Exercicio2.php
+++ Exercicio2.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,13 +5,30 @@
     </head>
     <body>
         <table border="1">
+            <tr>
+                <td><b>ID</b></td>
+                <td><b>NOME</b></td>
+                <td><b>DESC</b></td>
+            </tr>
             <?php
-            for($linha=1; $linha<=5; $linha++){
+            for($linha=1; $linha<=7; $linha++){
                 echo '<tr>';
-                for($coluna = 1; $coluna<= 2; $coluna++){
-                    echo '<td>';
-                    echo 'linha'.$linha;
-                    echo 'coluna'.$coluna;
+                for($coluna = 1; $coluna<= 3; $coluna++){
+                    if($linha % 2 == 0){
+                        echo '<td bgcolor = "grey">';
+                    }
+                    else{
+                        echo '<td>';
+                    }
+                    if($coluna == 1){
+                        echo 'id';
+                    }
+                    else if($coluna == 2){
+                        echo 'nome';
+                    }
+                    else if($coluna == 3){
+                        echo 'desc';
+                    }
                     echo '</td>';
                 }
                 echo '</tr>';
